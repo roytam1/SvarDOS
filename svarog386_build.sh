beb6ab51cacd6cb527f110d8d6c63cd9bfc5d79d
mv_fox <mv_fox@911cea91-c70f-4353-bd03-772f58fe8c9d>
2016-08-27 08:31:01 +0000

    build script generates ISO files with dates in filename, it's also configurable with a few settings now
    git-svn-id: svn://svn.svardos.org/svardos@11 911cea91-c70f-4353-bd03-772f58fe8c9d

--- svarog386_build.sh
+++ svarog386_build.sh
@@ -1,8 +1,23 @@
 #!/bin/sh
+#
+# This script generates indexes of Svarog386 repositories and builds the ISO
+# CD images. It should be executed each time that a package file have been
+# modified, added or removed from any of the repositories.
+#
+
+### parameters block starts here ############################################
 
 REPOROOT=/srv/www/svarog386.viste.fr/repos/
 BUILDIDX=/root/fdnpkg-buildidx/buildidx
+CDISODIR='/srv/www/svarog386.viste.fr/'
+CDROOT='/root/svarog386/cdroot'
 
+### parameters block ends here ##############################################
+
+# remember where we are, so we can return there once all is done
+origdir=`pwd`
+
+# first go to the root of repositories and refresh'em all
 cd $REPOROOT
 rm listing.txt
 $BUILDIDX base
@@ -25,8 +40,16 @@ $BUILDIDX sound
 if [ $? -ne 0 ] ; then exit 1 ; fi
 $BUILDIDX util
 
-genisoimage -input-charset cp437 -b boot.img -iso-level 1 -f -o ../svarog386.iso cdroot
-cd ..
-md5sum svarog386.iso > svarog386.iso.md5
+# compute a filename for the ISO file and build it
+CDISO="$CDISODIR/svarog386-`date +%Y%m%d-%H%M`.iso"
+genisoimage -input-charset cp437 -b boot.img -iso-level 1 -f -o $CDISO.tmp $CDROOT
+if [ $? -ne 0 ] ; then exit 1 ; fi
+mv $CDISO.tmp $CDISO
+
+# compute the MD5 of the ISO file, taking care to include only the filename in it
+cd `dirname $CDISO`
+md5sum `basename $CDISO` > $CDISO.md5
+
+cd "$origdir"
 
 exit 0
