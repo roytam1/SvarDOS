9060b71d69a2df071e9a22503346e90bb32dbe44
mv_fox <mv_fox@911cea91-c70f-4353-bd03-772f58fe8c9d>
2016-08-27 09:16:29 +0000

    added a link to the online repos art on main page, and simplified a few urls in links
    git-svn-id: svn://svn.svardos.org/svardos@15 911cea91-c70f-4353-bd03-772f58fe8c9d

--- website/index-main.php
+++ website/index-main.php
@@ -3,7 +3,7 @@
     <img src="svarog386.png" alt="logo" style="height: 10em; float: right; margin: 0 0 0.5em 0.5em;">
 
     <p>Svarog386 is a DOS distribution based on FreeDOS. It's released in the form of a single bootable CD image (ISO) that contains the FreeDOS kernel, a command interpreter and a variety of third-party packages.<br>
-    Svarog386 is a "rolling release", meaning that it doesn't follow the concept of "versions". Svarog386 can be kept up-to-date either via online update repositories or by using the latest ISO image as your local package repository.</p>
+    Svarog386 is a "rolling release", meaning that it doesn't follow the concept of "versions". Svarog386 can be kept up-to-date either via online <a href="?p=tech&amp;art=onlinerepos">update repositories</a> or by using the latest ISO image as your local package repository.</p>
 
     <p>Svarog386 is <b>not</b> designed for strict 8086 compatibility. Many parts of it might require a 386-class CPU. If you look for a simple 8086 FreeDOS distribution, take a look at <a href="http://svarog86.sourceforge.net">Svarog86</a>.</p>
 
@@ -20,13 +20,13 @@
   $fsize = filesize($latest_iso) >> 20;
   $ftime = date("d M Y", $files[$latest_iso]);
 
-  echo "    <p style=\"margin: 1.2em auto 0 auto; font-size: 1.2em; text-align: center; font-weight: bold;\"><a href=\"http://svarog386.viste.fr/{$latest_iso}\">Download the latest Svarog386 ISO</a></p>\n";
+  echo "    <p style=\"margin: 1.2em auto 0 auto; font-size: 1.2em; text-align: center; font-weight: bold;\"><a href=\"/{$latest_iso}\">Download the latest Svarog386 ISO</a></p>\n";
   echo '    <p style="margin: 0 auto 1.4em auto; font-size: 1em; text-align: center; color: #333;">';
 
-  echo "({$fsize}M, last update: {$ftime}, <a href=\"{$latest_iso}.md5\" style=\"color: inherit;\">MD5</a>)";
+  echo "({$fsize}M, last update: {$ftime}, <a href=\"/{$latest_iso}.md5\" style=\"color: inherit;\">MD5</a>)";
 
   echo "</p>\n";
 
 ?>
 
-    <p>You might want to know what it is exactly that you are about to download, before fetching the 450M+ ISO file. Feel free to browse the <a href="http://svarog386.viste.fr/repos/listing.txt">listing</a> of all the packages that come with Svarog386.</p>
+    <p>You might want to know what it is exactly that you are about to download, before fetching the 450M+ ISO file. Feel free to browse the <a href="/repos/listing.txt">listing</a> of all the packages that come with Svarog386.</p>
