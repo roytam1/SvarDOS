b02dba66178e183bb62f434f6fb8de14baad7e64
mv_fox <mv_fox@911cea91-c70f-4353-bd03-772f58fe8c9d>
2016-11-16 18:33:01 +0000

    created polish translations
    git-svn-id: svn://svn.svardos.org/svardos@58 911cea91-c70f-4353-bd03-772f58fe8c9d

---
+++ install/nls/install.pl
@@ -0,0 +1,39 @@
+#
+# This is a localization file for the Svarog386 INSTALL program
+#
+# Language..: Polish
+# Translator: Mateusz Viste
+#
+
+### COMMON STUFF: TITLE BAR AND MULTIPLE CHOICE STRINGS ###
+0.0:INSTALACJA SVAROG386
+0.1:Instaluj Svarog386
+0.2:Wyjd¶ do DOS
+0.3:Stw¢rz partycjë automatycznie
+0.4:Uruchom narzëdzie partycjonowania FDISK
+0.5:Naciûnij dowolny klawisz...
+0.6:Formatuj
+
+### LANGUAGE SELECTION SCREEN ###
+1.0:Witaj w systemie Svarog386
+1.1:Wybierz sw¢j jëzyk z ponißszej listy:
+
+### WELCOME SCREEN ###
+2.0:Za chwilë rozpocznie sië instalacja systemu Svarog386: bezpíatnego systemu operacyjnego opartego na jÜdrze FreeDOS, i kompatybilnego z MSDOS. Svarog386 jest przeznaczony dla komputer¢w klasy 386 wzwyß, i zawiera mn¢stwo aplikacji dodatkowych.\n\nUWAGA: Jeûli tw¢j komputer posiada juß inny system operacyjny, system ten moße nie zdoíaç sië uruchomiç po instalacji Svarog386.
+
+### DISK SETUP ###
+3.0:BúèD: Nie wykryto dysku %c:. Byç moße tw¢j dysk powinien zostaç najpierw spartycjonowany. Stw¢rz proszë co najmniej jednÜ partycjë na twoim dysku, aby Svarog386 m¢gí zostaç na niÜ zainstalowany. Svarog386 wymaga co najmniej %d MiB miejsca.\n\nMoßesz skorzystaç z narzëdzia FDISK aby stworzyç wymaganÜ partycjë rëcznie, lub moßesz pozwoliç instalatorowi stworzyç jÜ automatycznie. Moßesz takße wstrzymaç instalacjë aby ußyç jakiegokolwiek innego narzëdzia do partycjonowania.
+3.1:Tw¢j komputer zostanie teraz uruchomiony ponownie.
+3.2:BúèD: Dysk %c: jest noûnikiem wymiennym. Instalacja zatrzymana.
+3.3:BúèD: Dysk %c: zdaje sië nie byç sformatowany. Czy chcesz go sformatowaç?
+3.4:BúèD: Dysk %c: jest za maíy! Svarog386 wymaga co najmniej %d MiB miejsca.
+3.5:BúèD: Dysk %c: nie jest pusty. Svarog386 moße byç zainstalowany wyíÜcznie na pustym dysku.\n\n
+Moßesz sformatowaç dysk teraz, aby go opr¢ßniç. Operacja ta sprawi jednak ße WSZYSTKIE OBECNE DANE ZOSTANè UTRACONE.
+3.6:Za chwilë rozpocznie sië instalacja systemu Svarog386 na dysk %c.
+
+### PACKAGES INSTALLATION ###
+# example: "Installing package 4/50: FDISK"
+4.0:Instalacja pakietu %d/%d: %s
+
+### END SCREEN ###
+5.0:Instalacja Svarog386 jest zako§czona. Tw¢j komputer zostanie teraz uruchomiony ponownie.\nWyjmij proszë dysk instalacyjny z napëdu.
