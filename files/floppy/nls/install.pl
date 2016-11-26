ff2d0c179b31b77d691af7a459b5bbeebe90eee9
mv_fox <mv_fox@911cea91-c70f-4353-bd03-772f58fe8c9d>
2016-11-26 12:56:13 +0000

    synced latest build of the install program to the boot floppy
    git-svn-id: svn://svn.svardos.org/svardos@82 911cea91-c70f-4353-bd03-772f58fe8c9d

--- files/floppy/nls/install.pl
+++ files/floppy/nls/install.pl
@@ -13,6 +13,9 @@
 0.4:Uruchom narzëdzie partycjonowania FDISK
 0.5:Naciûnij dowolny klawisz...
 0.6:Formatuj
+# Every string below must be at most 78 characters long! (used in status bar)
+0.10:G¢ra/D¢í = Wyb¢r pozycji | Enter = Zatwierd¶ wyb¢r | ESC = Poprzedni ekran
+0.11:G¢ra/D¢í = Wyb¢r pozycji | Enter = Zatwierd¶ wyb¢r | ESC = Wyjd¶ do DOS
 
 ### LANGUAGE SELECTION & KEYBOARD LAYOUT SCREENS ###
 1.0:Witaj w systemie Svarog386
@@ -20,7 +23,7 @@
 1.5:Svarog386 wspiera ukíady klawiatur wspierane w r¢ßnych krajach. Wybierz ukíad klawiatury kt¢ry ci odpowiada.
 
 ### WELCOME SCREEN ###
-2.0:Za chwilë rozpocznie sië instalacja systemu Svarog386: bezpíatnego systemu operacyjnego opartego na jÜdrze FreeDOS, i kompatybilnego z MSDOS. Svarog386 jest przeznaczony dla komputer¢w klasy 386 wzwyß, i zawiera mn¢stwo aplikacji dodatkowych.\n\nUWAGA: Jeûli tw¢j komputer posiada juß inny system operacyjny, system ten moße nie zdoíaç sië uruchomiç po instalacji Svarog386.
+2.0:Za chwilë rozpocznie sië instalacja systemu Svarog386: bezpíatnego systemu operacyjnego opartego na jÜdrze FreeDOS, i kompatybilnego z MSDOS. Svarog386 jest przeznaczony dla komputer¢w klasy 386 wzwyß, i zawiera mn¢stwo aplikacji dodatkowych.\n\nUWAGA: Jeûli tw¢j komputer posiada juß inny system operacyjny, system ten moße nie zdoíaç uruchomiç sië po instalacji Svarog386.
 
 ### DISK SETUP ###
 3.0:BúèD: Nie wykryto dysku %c:. Byç moße tw¢j dysk powinien zostaç najpierw spartycjonowany. Utw¢rz co najmniej jednÜ partycjë na twoim dysku, aby Svarog386 m¢gí zostaç na niÜ zainstalowany. Svarog386 wymaga co najmniej %d MiB miejsca.\n\nMoßesz skorzystaç z narzëdzia FDISK aby stworzyç wymaganÜ partycjë rëcznie, lub pozwoliç instalatorowi stworzyç jÜ automatycznie. Moßesz takße wstrzymaç instalacjë aby ußyç jakiegokolwiek innego narzëdzia do partycjonowania.
