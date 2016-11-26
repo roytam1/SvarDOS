4ad558f4e12aac848f91bc9bb6f6d4fcea7b6e43
mv_fox <mv_fox@911cea91-c70f-4353-bd03-772f58fe8c9d>
2016-11-26 17:08:07 +0000

    published the "micro" version and added a note about floppy boot
    git-svn-id: svn://svn.svardos.org/svardos@86 911cea91-c70f-4353-bd03-772f58fe8c9d

--- website/index-main.php
+++ website/index-main.php
@@ -4,12 +4,12 @@
     <div style="overflow: auto;">
     <img src="svarog386.png" alt="logo" style="height: 10em; float: right; margin: 0 0 0.5em 0.5em;">
 
-    <p>Svarog386 is a DOS distribution based on FreeDOS. It's released in the form of a single bootable CD image (ISO) that contains the FreeDOS kernel, a command interpreter and a variety of third-party packages.<br>
+    <p>Svarog386 is a free, multilingual DOS distribution based on FreeDOS. It's released in the form of a single bootable CD image (ISO) that contains the FreeDOS kernel, a command interpreter and a variety of third-party packages.<br>
     Svarog386 is a "rolling release", meaning that it doesn't follow the concept of "versions". Svarog386 can be kept up-to-date either via online <a href="?p=tech&amp;art=onlinerepos">update repositories</a> or by using the latest ISO image as your local package repository.</p>
 
     <p>Svarog386 is <b>not</b> designed for strict 8086 compatibility. Many parts of it might require a 386-class CPU. If you look for a simple 8086 FreeDOS distribution, take a look at <a href="http://svarog86.sourceforge.net">Svarog86</a>.</p>
 
-    <p>Need to get in touch, or wish to contribute some packages? Feel free to drop a line to the usenet group <b>alt.os.free-dos</b>.
+    <p>Need to get in touch? Wish to contribute some packages, translate Svarog386 to your language, or otherwise contribute? Feel free to drop a line or two to the usenet group <b>alt.os.free-dos</b>.
     </div>
 
 <?php
@@ -52,6 +52,13 @@
   echo "      <p style=\"margin: 0 auto 1.4em auto; font-size: 1em; text-align: center; color: #333;\">({$fsize_full}M, last update: {$ftime_full}, <a href=\"/{$latest_iso_full}.md5\" style=\"color: inherit;\">MD5</a>)</p>\n";
   echo "    </div>\n";
 
+  echo "    <div style=\"width: 100%; margin: 1.2em 0 0 0;\">\n";
+  echo "      <p style=\"text-align: center; font-weight: bold;\"><a href=\"/{$latest_iso_micro}\">Download Svarog386 micro (core OS only)</a></p>\n";
+  echo "      <p style=\"margin: 0 auto 1.4em auto; font-size: 1em; text-align: center; color: #333;\">({$fsize_micro}M, last update: {$ftime_micro}, <a href=\"/{$latest_iso_micro}.md5\" style=\"color: inherit;\">MD5</a>)</p>\n";
+  echo "    </div>\n";
+
 ?>
 
     <p>You might want to know what it is exactly that you are about to download, before fetching the multi-megabytes ISO file. Feel free to browse the <a href="listing.txt">listing</a> of all the packages that come with Svarog386.</p>
+
+    <p>If your computer is unable to boot from a CD, you can use Svarog386's <a href="boot.img">boot floppy image</a> to install Svarog386 (you still need to put Svarog386's CD in your drive).</p>
