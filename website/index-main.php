3ac725ca1c082a88661932ddfcf40ac194914934
mv_fox <mv_fox@911cea91-c70f-4353-bd03-772f58fe8c9d>
2016-11-26 15:51:32 +0000

    small wording improvements to the website
    git-svn-id: svn://svn.svardos.org/svardos@83 911cea91-c70f-4353-bd03-772f58fe8c9d

--- website/index-main.php
+++ website/index-main.php
@@ -9,7 +9,7 @@
 
     <p>Svarog386 is <b>not</b> designed for strict 8086 compatibility. Many parts of it might require a 386-class CPU. If you look for a simple 8086 FreeDOS distribution, take a look at <a href="http://svarog86.sourceforge.net">Svarog86</a>.</p>
 
-    <p>Need to get in touch, or wish to contribute some packages? Feel free to drop a line to the usenet group <b>alt.os.free-dos</b>. Alternatively, you could also reach me directly via <a href="https://sourceforge.net/u/userid-1220451/">sourceforge</a>.
+    <p>Need to get in touch, or wish to contribute some packages? Feel free to drop a line to the usenet group <b>alt.os.free-dos</b>.
     </div>
 
 <?php
@@ -31,18 +31,27 @@
   $fsize_nosrc = filesize($latest_iso_nosrc) >> 20;
   $ftime_nosrc = date("d M Y", $files[$latest_iso_nosrc]);
 
+  // find the latest 'micro' ISO available
+  $files = glob("svarog386-micro-*.iso", GLOB_NOSORT);
+  $files = array_combine($files, array_map("filemtime", $files));
+  arsort($files);
+  $latest_iso_micro = key($files);
+  // compute file size and date of the 'micro' ISO
+  $fsize_micro = filesize($latest_iso_micro) >> 20;
+  $ftime_micro = date("d M Y", $files[$latest_iso_micro]);
+
   //echo "    <p style=\"margin: 1.2em auto 0 auto; font-size: 1.2em; text-align: center; font-weight: bold;\">Download the latest Svarog386 ISO</p>\n";
 
-  echo "    <div style=\"width: 45%; float: left; margin: 1.2em 0 0 5%;\">\n";
-  echo "      <p style=\"text-align: center; font-weight: bold;\"><a href=\"/{$latest_iso_full}\">Download Svarog386 full version</a></p>\n";
-  echo "      <p style=\"margin: 0 auto 1.4em auto; font-size: 1em; text-align: center; color: #333;\">({$fsize_full}M, last update: {$ftime_full}, <a href=\"/{$latest_iso_full}.md5\" style=\"color: inherit;\">MD5</a>)</p>\n";
+  echo "    <div style=\"width: 45%; float: left; margin: 1.2em 5% 0 0;\">\n";
+  echo "      <p style=\"text-align: center; font-weight: bold;\"><a href=\"/{$latest_iso_nosrc}\">Download Svarog386</a></p>\n";
+  echo "      <p style=\"margin: 0 auto 1.4em auto; font-size: 1em; text-align: center; color: #333;\">({$fsize_nosrc}M, last update: {$ftime_nosrc}, <a href=\"/{$latest_iso_nosrc}.md5\" style=\"color: inherit;\">MD5</a>)</p>\n";
   echo "    </div>\n";
 
-  echo "    <div style=\"width: 45%; float: right; margin: 1.2em 5% 0 0;\">\n";
-  echo "      <p style=\"text-align: center; font-weight: bold;\"><a href=\"/{$latest_iso_nosrc}\">Download Svarog386 binary (no sources)</a></p>\n";
-  echo "      <p style=\"margin: 0 auto 1.4em auto; font-size: 1em; text-align: center; color: #333;\">({$fsize_nosrc}M, last update: {$ftime_nosrc}, <a href=\"/{$latest_iso_nosrc}.md5\" style=\"color: inherit;\">MD5</a>)</p>\n";
+  echo "    <div style=\"width: 45%; float: right; margin: 1.2em 0 0 5%;\">\n";
+  echo "      <p style=\"text-align: center; font-weight: bold;\"><a href=\"/{$latest_iso_full}\">Download Svarog386 (+sources)</a></p>\n";
+  echo "      <p style=\"margin: 0 auto 1.4em auto; font-size: 1em; text-align: center; color: #333;\">({$fsize_full}M, last update: {$ftime_full}, <a href=\"/{$latest_iso_full}.md5\" style=\"color: inherit;\">MD5</a>)</p>\n";
   echo "    </div>\n";
 
 ?>
 
-    <p>You might want to know what it is exactly that you are about to download, before fetching the 450M+ ISO file. Feel free to browse the <a href="listing.txt">listing</a> of all the packages that come with Svarog386.</p>
+    <p>You might want to know what it is exactly that you are about to download, before fetching the multi-megabytes ISO file. Feel free to browse the <a href="listing.txt">listing</a> of all the packages that come with Svarog386.</p>
