3f369b68c130410e25a2ae31aad4c4738c7679af
mv_fox <mv_fox@911cea91-c70f-4353-bd03-772f58fe8c9d>
2016-12-13 13:45:50 +0000

    added utf-8 versions of each translation file, as well as russian strings (translated by me as best effort) and a convertion script
    git-svn-id: svn://svn.svardos.org/svardos@114 911cea91-c70f-4353-bd03-772f58fe8c9d

---
+++ install/nls/recompute.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+utf8tocp maz install.pl.utf8 > install.pl
+if [ $? -ne 0 ] ; then
+  echo "ERROR! ABORTED!"; exit
+fi
+
+utf8tocp 858 install.fr.utf8 > install.fr
+if [ $? -ne 0 ] ; then
+  echo "ERROR! ABORTED!"; exit
+fi
+
+utf8tocp 857 install.tr.utf8 > install.tr
+if [ $? -ne 0 ] ; then
+  echo "ERROR! ABORTED!"; exit
+fi
+
+utf8tocp 866 install.ru.utf8 > install.ru
+if [ $? -ne 0 ] ; then
+  echo "ERROR! ABORTED!"; exit
+fi
