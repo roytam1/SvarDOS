975f0dfb9cced5ef623cfcb340f1f83d39694a32
mv_fox <mv_fox@911cea91-c70f-4353-bd03-772f58fe8c9d>
2016-12-15 18:22:57 +0000

    added support for the italian language
    git-svn-id: svn://svn.svardos.org/svardos@119 911cea91-c70f-4353-bd03-772f58fe8c9d

--- install/nls/recompute.sh
+++ install/nls/recompute.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+utf8tocp 858 install.it.utf8 > install.it
+if [ $? -ne 0 ] ; then
+  echo "ERROR! ABORTED!"; exit
+fi
+
 utf8tocp maz install.pl.utf8 > install.pl
 if [ $? -ne 0 ] ; then
   echo "ERROR! ABORTED!"; exit
