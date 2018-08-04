03e741735e14481f74e279103353b7e184995f58
mv_fox <mv_fox@911cea91-c70f-4353-bd03-772f58fe8c9d>
2018-08-04 19:22:16 +0000

    improved build process
    git-svn-id: svn://svn.svardos.org/svardos@148 911cea91-c70f-4353-bd03-772f58fe8c9d

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 #
+# Svarog386 build script
+# http://svarog386.sourceforge.net
+# Copyright (C) 2016-2018 Mateusz Viste
+#
 # This script generates indexes of Svarog386 repositories and builds the ISO
 # CD images. It should be executed each time that a package file have been
 # modified, added or removed from any of the repositories.
@@ -7,22 +11,22 @@
 
 ### parameters block starts here ############################################
 
-REPOROOT='/srv/www/svarog386.viste.fr/repos/'
-REPOROOTSRC='/srv/www/svarog386.viste.fr/repos-src/'
-REPOROOTNOSRC='/srv/www/svarog386.viste.fr/repos-nosrc/'
-BUILDIDX='/root/fdnpkg-buildidx/buildidx'
-CDISODIR='/srv/www/svarog386.viste.fr/'
-CDISODIRTEST='/srv/www/svarog386.viste.fr/test/'
-CDROOT='/root/svarog386/cdroot'
-CDROOTNOSRC='/root/svarog386/cdrootnosrc'
-CDROOTMICRO='/root/svarog386/cdrootmicro'
-CUSTFILES='/root/svarog386/files'
+REPOROOT=`realpath ./website/repos/`
+REPOROOTSRC=`realpath ./website/repos-src/`
+REPOROOTNOSRC=`realpath ./website/repos-nosrc/`
+BUILDIDX=`realpath ../fdnpkg/trunk/buildidx/buildidx`
+CDISODIR=`realpath ./iso/`
+PUBDIR=`realpath ./website/`
+CDROOT=`realpath ./cdroot`
+CDROOTNOSRC=`realpath ./cdrootnosrc`
+CDROOTMICRO=`realpath ./cdrootmicro`
+CUSTFILES=`realpath ./files`
 
 ### parameters block ends here ##############################################
 
 # function to be called with the repository (short) name as argument
 function dorepo {
-  repodir=$1
+  repodir="$1"
   # switch to repo's dir
   cd $REPOROOT/$repodir
   # duplicate all zip files as zib
@@ -36,15 +40,15 @@ function dorepo {
   find $REPOROOT/$repodir -iname '*.zib' -exec zip "{}" -d "Source/*" ';'
 
   # build idx for the full (net) repo
-  $BUILDIDX $REPOROOT/$repodir
+  $BUILDIDX "$REPOROOT/$repodir"
   # copy listing into the global listing
-  cat $REPOROOT/$repodir/listing.txt >> $CDISODIR/listing.txt
+  cat "$REPOROOT/$repodir/listing.txt" >> "$PUBDIR/listing.txt"
 
   # compute links for the 'no source' version
   mkdir -p $REPOROOTNOSRC/$repodir
   cd $REPOROOTNOSRC/$repodir
   if [ $? -ne 0 ] ; then exit 1 ; fi
-  rm *.zip *.zib
+  rm *.zip
   for f in $REPOROOT/$repodir/*.zib
   do
   ln -s "$f" `basename -s .zib $f`.zip
@@ -56,7 +60,7 @@ function dorepo {
   mkdir -p $REPOROOTSRC/$repodir
   cd $REPOROOTSRC/$repodir
   if [ $? -ne 0 ] ; then exit 1 ; fi
-  rm *.zip *.zib
+  rm *.zip
   for f in $REPOROOT/$repodir/*.zip
   do
   ln -s "$f" `basename $f`
@@ -67,37 +71,31 @@ function dorepo {
 
 ### actual code flow starts here ############################################
 
-
-TESTMODE=0
-if [ "x$1" = "xprod" ]; then
-  TESTMODE=0
-elif [ "x$1" = "xtest" ]; then
-  TESTMODE=1
-  CDISODIR="$CDISODIRTEST"
-  mkdir -p "$CDISODIR"
-else
-  echo "usage: build.sh prod|test"
+# check presence of the buildidx tool
+if [ ! -f "$BUILDIDX" ] ; then
+  echo "buildidx not found at $BUILDIDX"
   exit 1
 fi
 
-# remember where we are, so we can return there once all is done
+# remember where I am, so I can get back here once all is done
 origdir=`pwd`
 
 # build the boot floppy image first
 cp $CUSTFILES/bootmini.img $CDROOT/boot.img
 export MTOOLS_NO_VFAT=1
-mcopy -sQm -i $CDROOT/boot.img $CUSTFILES/floppy/* ::/
+mcopy -sQm -i "$CDROOT/boot.img" $CUSTFILES/floppy/* ::/
+if [ $? -ne 0 ] ; then exit 1 ; fi
 
 # sync the boot.img file from full version to nosrc and micro, and publish it also stand-alone
-cp $CDROOT/boot.img $CDROOTNOSRC/
+cp "$CDROOT/boot.img" "$CDROOTNOSRC/"
 if [ $? -ne 0 ] ; then exit 1 ; fi
-cp $CDROOT/boot.img $CDROOTMICRO/
+cp "$CDROOT/boot.img" "$CDROOTMICRO/"
 if [ $? -ne 0 ] ; then exit 1 ; fi
-cp $CDROOT/boot.img $CDISODIR/
+cp "$CDROOT/boot.img" "$PUBDIR/"
 if [ $? -ne 0 ] ; then exit 1 ; fi
 
 # remove the 'human' listing (will be regenerated in a short moment)
-rm $CDISODIR/listing.txt
+rm "$PUBDIR/listing.txt"
 
 # process all repos (also builds the listing.txt file)
 dorepo core
@@ -111,36 +109,54 @@ dorepo packers
 dorepo sound
 dorepo util
 
+# delete all (previous) *.iso and *.md5 files
+echo "cleaning up old versions..."
+rm $CDISODIR/svarog386-*-*.iso*
 
 # compute a filename for the ISO files and build it
 DATESTAMP=`date +%Y%m%d-%H%M`
-CDISO="$CDISODIR/svarog386-full-$DATESTAMP.iso"
-CDISONOSRC="$CDISODIR/svarog386-nosrc-$DATESTAMP.iso"
-CDISOMICRO="$CDISODIR/svarog386-micro-$DATESTAMP.iso"
-genisoimage -input-charset cp437 -b boot.img -iso-level 1 -f -V SVAROG386 -o $CDISO.tmp $CDROOT
+CDISO="$CDISODIR/svarog386-$DATESTAMP-full.iso"
+CDISONOSRC="$CDISODIR/svarog386-$DATESTAMP-nosrc.iso"
+CDISOMICRO="$CDISODIR/svarog386-$DATESTAMP-micro.iso"
+genisoimage -input-charset cp437 -b boot.img -iso-level 1 -f -V SVAROG386 -o "$CDISO" "$CDROOT"
 if [ $? -ne 0 ] ; then exit 1 ; fi
-genisoimage -input-charset cp437 -b boot.img -iso-level 1 -f -V SVAROG386 -o $CDISONOSRC.tmp $CDROOTNOSRC
+genisoimage -input-charset cp437 -b boot.img -iso-level 1 -f -V SVAROG386 -o "$CDISONOSRC" "$CDROOTNOSRC"
 if [ $? -ne 0 ] ; then exit 1 ; fi
-genisoimage -input-charset cp437 -b boot.img -iso-level 1 -f -V SVAROG386 -o $CDISOMICRO.tmp $CDROOTMICRO
+genisoimage -input-charset cp437 -b boot.img -iso-level 1 -f -V SVAROG386 -o "$CDISOMICRO" "$CDROOTMICRO"
 if [ $? -ne 0 ] ; then exit 1 ; fi
-mv $CDISO.tmp $CDISO
-mv $CDISONOSRC.tmp $CDISONOSRC
-mv $CDISOMICRO.tmp $CDISOMICRO
 
 # compute the MD5 of the ISO files, taking care to include only the filename in it
 echo "computing md5 sums..."
-cd `dirname $CDISO`
-md5sum `basename $CDISO` > $CDISO.md5
+cd `dirname "$CDISO"`
+md5sum `basename "$CDISO"` > "$CDISO.md5"
+if [ $? -ne 0 ] ; then exit 1 ; fi
 
-cd `dirname $CDISONOSRC`
-md5sum `basename $CDISONOSRC` > $CDISONOSRC.md5
+cd `dirname "$CDISONOSRC"`
+md5sum `basename "$CDISONOSRC"` > "$CDISONOSRC.md5"
+if [ $? -ne 0 ] ; then exit 1 ; fi
 
-cd `dirname $CDISOMICRO`
-md5sum `basename $CDISOMICRO` > $CDISOMICRO.md5
+cd `dirname "$CDISOMICRO"`
+md5sum `basename "$CDISOMICRO"` > "$CDISOMICRO.md5"
+if [ $? -ne 0 ] ; then exit 1 ; fi
 
-# delete all *.iso and *.md5 files, leaving only the 16 most recent
-echo "cleaning up old versions..."
-ls -tp $CDISODIR/svarog386-*-*.iso* | tail -n +17 | xargs -I {} rm -- {}
+# compute the ini file with properties of each ISO
+echo "[micro]" > "$PUBDIR/downloads.ini"
+echo "url=\"https://sourceforge.net/projects/svarog386/files/svarog386-$DATESTAMP-micro.iso/download\"" >> "$PUBDIR/downloads.ini"
+echo "md5=\"https://sourceforge.net/projects/svarog386/files/svarog386-$DATESTAMP-micro.md5/download\"" >> "$PUBDIR/downloads.ini"
+echo "size=`stat --format='%s' $CDISOMICRO`" >> "$PUBDIR/downloads.ini"
+echo "date=`stat --format='%Y' $CDISOMICRO`" >> "$PUBDIR/downloads.ini"
+echo "" >> "$PUBDIR/downloads.ini"
+echo "[full]" >> "$PUBDIR/downloads.ini"
+echo "url=\"https://sourceforge.net/projects/svarog386/files/svarog386-$DATESTAMP-full.iso/download\"" >> "$PUBDIR/downloads.ini"
+echo "md5=\"https://sourceforge.net/projects/svarog386/files/svarog386-$DATESTAMP-full.md5/download\"" >> "$PUBDIR/downloads.ini"
+echo "size=`stat --format='%s' $CDISO`" >> "$PUBDIR/downloads.ini"
+echo "date=`stat --format='%Y' $CDISO`" >> "$PUBDIR/downloads.ini"
+echo "" >> "$PUBDIR/downloads.ini"
+echo "[nosrc]" >> "$PUBDIR/downloads.ini"
+echo "url=\"https://sourceforge.net/projects/svarog386/files/svarog386-$DATESTAMP-nosrc.iso/download\"" >> "$PUBDIR/downloads.ini"
+echo "md5=\"https://sourceforge.net/projects/svarog386/files/svarog386-$DATESTAMP-nosrc.md5/download\"" >> "$PUBDIR/downloads.ini"
+echo "size=`stat --format='%s' $CDISONOSRC`" >> "$PUBDIR/downloads.ini"
+echo "date=`stat --format='%Y' $CDISONOSRC`" >> "$PUBDIR/downloads.ini"
 
 cd "$origdir"
 
