a57f64342fe3ffde240d495b5981a0d21c3b61fa
mv_fox <mv_fox@911cea91-c70f-4353-bd03-772f58fe8c9d>
2018-08-05 19:51:57 +0000

    website is synchronized with --delete
    git-svn-id: svn://svn.svardos.org/svardos@166 911cea91-c70f-4353-bd03-772f58fe8c9d

--- sync.sh
+++ sync.sh
@@ -12,4 +12,4 @@ rsync -a --progress iso/* mv_fox@frs.sourceforge.net:/home/frs/project/svarog386
 if [ $? -ne 0 ] ; then exit 1 ; fi
 
 # sync the website (with repositories)
-rsync -a --progress website/* mv_fox@web.sourceforge.net:/home/project-web/svarog386/htdocs/
+rsync -a --delete --progress website/ mv_fox@web.sourceforge.net:/home/project-web/svarog386/htdocs
