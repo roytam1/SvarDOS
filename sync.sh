c99344a39ec68c3c22aeb7c6c968d07d8848f46c
mv_fox <mv_fox@911cea91-c70f-4353-bd03-772f58fe8c9d>
2018-08-05 17:08:30 +0000

    sync.sh is used to synchronize a local build towards the website and sourceforge download area
    git-svn-id: svn://svn.svardos.org/svardos@164 911cea91-c70f-4353-bd03-772f58fe8c9d

---
+++ sync.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#
+# Svarog386 build script
+# http://svarog386.sourceforge.net
+# Copyright (C) 2016-2018 Mateusz Viste
+#
+# Synchronization of ISO files, website and repositories towards sourceforge.
+#
+
+# sync ISO files to sourceforge download servers
+rsync -a --progress iso/* mv_fox@frs.sourceforge.net:/home/frs/project/svarog386/
+if [ $? -ne 0 ] ; then exit 1 ; fi
+
+# sync the website (with repositories)
+rsync -a --progress website/* mv_fox@web.sourceforge.net:/home/project-web/svarog386/htdocs/
