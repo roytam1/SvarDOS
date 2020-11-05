837b86aed3e952652330d6c925933dae35a50c46
mv_fox <mv_fox@911cea91-c70f-4353-bd03-772f58fe8c9d>
2020-11-05 14:39:57 +0000

    use eltorito as a last resort only -- under virtualbox 6.0 it yields a seemingly empty drive
    git-svn-id: svn://svn.svardos.org/svardos@170 911cea91-c70f-4353-bd03-772f58fe8c9d

--- files/floppy/autoexec.bat
+++ files/floppy/autoexec.bat
@@ -12,10 +12,16 @@ REM DISPLAY driver for NLS support
 DISPLAY CON=(EGA,,1)
 
 REM *** CDROM initialization ***
-IF NOT EXIST SVCD0001 DEVLOAD /Q /H A:\ELTORITO.SYS /D:SVCD0001
+
 REM UDVD2 is called with /UX to DISABLE UltroDMA (force PIO mode). Otherwise
 REM freezes happen when running VirtualBox (tested under vbox 5.0 and 5.1)
 IF NOT EXIST SVCD0001 DEVLOAD /Q /H A:\UDVD2.SYS /D:SVCD0001 /UX
+
+REM if UDVD2 failed, try using eltorito as last resort
+REM under Virtualbox 6.0 eltorito leads to an empty drive
+IF NOT EXIST SVCD0001 DEVLOAD /Q /H A:\ELTORITO.SYS /D:SVCD0001
+
+REM enable the CD only if driver succeeded
 IF EXIST SVCD0001 SHSUCDX /D:SVCD0001 /Q
 
 A:\FDAPM APMDOS
