88b881fdd69b8a5fe938d6d81f315fca521e3a35
mateuszviste <mateuszviste@911cea91-c70f-4353-bd03-772f58fe8c9d>
2021-01-11 12:32:00 +0000

    sync script relies on OSDN
    git-svn-id: svn://svn.svardos.org/svardos@176 911cea91-c70f-4353-bd03-772f58fe8c9d

--- sync.sh
+++ sync.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 #
-# Svarog386 build script
-# http://svarog386.sourceforge.net
-# Copyright (C) 2016-2018 Mateusz Viste
+# SvarDOS build script
+# http://svardos.osdn.io
+# Copyright (C) 2016-2021 Mateusz Viste
 #
 # Synchronization of ISO files, website and repositories towards sourceforge.
 #
 
 # sync ISO files to sourceforge download servers
-#rsync -a --progress iso/* mv_fox@frs.sourceforge.net:/home/frs/project/svarog386/
+#rsync -a --progress iso/* mateuszviste@storage.osdn.net:/storage/groups/s/sv/svardos/
 #if [ $? -ne 0 ] ; then exit 1 ; fi
 
 # sync the website (with repositories)
-rsync -a --delete --progress website/ mv_fox@web.sourceforge.net:/home/project-web/svarog386/htdocs
+rsync -a --delete --progress website/ mateuszviste@shell.osdn.net/home/groups/s/sv/svardos/htdocs/
+
