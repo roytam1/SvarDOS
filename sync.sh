4a33a80596b38cfd2caf2c0f32ce0893562ded9e
mateuszviste <mateuszviste@911cea91-c70f-4353-bd03-772f58fe8c9d>
2021-01-11 12:05:53 +0000

    disabled iso sync for now
    git-svn-id: svn://svn.svardos.org/svardos@173 911cea91-c70f-4353-bd03-772f58fe8c9d

--- sync.sh
+++ sync.sh
@@ -8,8 +8,8 @@
 #
 
 # sync ISO files to sourceforge download servers
-rsync -a --progress iso/* mv_fox@frs.sourceforge.net:/home/frs/project/svarog386/
-if [ $? -ne 0 ] ; then exit 1 ; fi
+#rsync -a --progress iso/* mv_fox@frs.sourceforge.net:/home/frs/project/svarog386/
+#if [ $? -ne 0 ] ; then exit 1 ; fi
 
 # sync the website (with repositories)
 rsync -a --delete --progress website/ mv_fox@web.sourceforge.net:/home/project-web/svarog386/htdocs
