e70725396aa65022fd43d3cecb868d6d1092d182
mateuszviste <mateuszviste@911cea91-c70f-4353-bd03-772f58fe8c9d>
2021-01-16 15:28:52 +0000

    skip CD initialization for floppy-only installs
    git-svn-id: svn://svn.svardos.org/svardos@199 911cea91-c70f-4353-bd03-772f58fe8c9d

--- files/floppy/autoexec.bat
+++ files/floppy/autoexec.bat
@@ -10,7 +10,9 @@ ALIAS HALT=FDAPM POWEROFF
 REM DISPLAY driver for NLS support
 DISPLAY CON=(EGA,,1)
 
-REM *** CDROM initialization ***
+REM *** CDROM initialization - ONLY IF REQUIRED FOR PACKAGES INSTALL ***
+
+IF EXIST A:\ATTRIB.ZIP GOTO SKIPCDINIT
 
 REM UDVD2 is called with /UX to DISABLE UltroDMA (force PIO mode). Otherwise
 REM freezes happen when running VirtualBox (tested under vbox 5.0 and 5.1)
@@ -22,6 +24,8 @@ IF NOT EXIST SVCD0001 DEVLOAD /Q /H A:\ELTORITO.SYS /D:SVCD0001
 
 REM enable the CD only if driver succeeded
 IF EXIST SVCD0001 SHSUCDX /D:SVCD0001 /Q
+
+:SKIPCDINIT
 
 FDAPM APMDOS
 
