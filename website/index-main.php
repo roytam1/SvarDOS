22bda6264033e6f280c3034271da46b596ec933c
mateuszviste <mateuszviste@911cea91-c70f-4353-bd03-772f58fe8c9d>
2021-01-23 13:54:12 +0000

    wording improvements
    git-svn-id: svn://svn.svardos.org/svardos@217 911cea91-c70f-4353-bd03-772f58fe8c9d

--- website/index-main.php
+++ website/index-main.php
@@ -1,28 +1,25 @@
     <h1>SvarDOS - an open-source DOS distribution</h1>
     <p class="copyr">brought to you by <a href="http://mateusz.viste.fr/" class="mateusz">Mateusz Viste</a></p>
 
-    <div style="margin: 2em auto; width: 20em; border: 1px #777 solid; background-color: #fff; padding: 0.5em;">
-    <p style="text-align: center; font-size: 1.5em; margin: 1.5em auto;">WORK IN PROGRESS</p>
-    <p style="font-size: 1.2em;">This project is in a work-in-progress phase, nothing works yet. Please come back later!</p>
-    </div>
+    <div style="margin: 2em auto; width: 21em; border: 1px #777 solid; background-color: #fff; padding: 0.1em 0.5em; color: #a00; font-size: 1.1em;">This project is very much "work-in-progress", not everything works yet!</div>
 
-    <p>SvarDOS is an open-source project that is meant to integrate the best out of the currently available DOS tools, drivers and games. DOS development has been abandoned by commercial players a very long time ago, mostly during early nineties. Nowadays, it survives solely through the efforts of hobbyists and retro-enthusiasts, but this is a highly sparse and unorganized ecosystem. SvarDOS aims to collect available DOS software, package it and make it easy to find and install applications using a network-enabled package manager.</p>
+    <p>SvarDOS is an open-source project that is meant to integrate the best out of the currently available DOS tools, drivers and games. DOS development has been abandoned by commercial players a very long time ago, mostly during early nineties. Nowadays, it survives solely through the efforts of hobbyists and retro-enthusiasts, but this is a highly sparse and unorganized ecosystem. SvarDOS aims to collect available DOS software, package it and make it easy to find and install applications using a network-enabled package manager (like apt-get, but for DOS and able to run even on a 8086 PC).</p>
 
-    <h2>SvarDOS is minimalist and 8086-compatibe</h2>
+    <h2>Minimalist and 8086-compatibe</h2>
 
     <p>Once installed, SvarDOS is a minimalistic DOS system that offers only the most basic tools for system administration. It is up to the user to install additional packages. Care is taken so SvarDOS remains 8086-compatible, at least in its most basic (core) configuration.</p>
 
-    <h2>SvarDOS is open-source</h2>
+    <h2>Open-source</h2>
 
-    <p>SvarDOS files are published under the terms of the MIT license. This applies only to SvarDOS-specific files, though - the packages may be subject to different licenses (GPL, BSD, Public Domain, Freeware...).</p>
+    <p>SvarDOS files are published under the terms of the MIT license. This applies only to SvarDOS-specific files, though - the packages supplied with SvarDOS may be subject to different licenses (GPL, BSD, Public Domain, Freeware...).</p>
 
-    <h2>SvarDOS has no "versions"</h2>
+    <h2>No "versions"</h2>
 
-    <p>SvarDOS is a "rolling" release, meaning that it doesn't follow the concept of "versions". Once installed, SvarDOS can be kept up-to-date trough its online update tool.</p>
+    <p>SvarDOS is a "rolling" release, meaning that it doesn't follow the concept of "versions". Once the system is installed, its packages can be kept up-to-date using the SvarDOS online update tools (pkgnet &amp; pkginst).</p>
 
-    <h2>The community</h2>
+    <h2>Community and help</h2>
 
-    <p>Need to get in touch? Wish to contribute some packages, translate SvarDOS to your language, or otherwise contribute? Or maybe you'd like some information about SvarDOS? The project has a <a href="https://lists.osdn.me/mailman/listinfo/svardos-users">mailing list</a> just for that.</p>
+    <p>Need to get in touch? Wish to submit some packages, translate SvarDOS to your language, or otherwise contribute? Or maybe you'd like some information about SvarDOS? The project has a <a href="https://lists.osdn.me/mailman/listinfo/svardos-users">mailing list</a> just for that.</p>
 
     <h2>Downloads</h2>
 
@@ -34,4 +31,4 @@
       <li><a href="download/svardos-floppy-720k.zip">SvarDOS install on 720K floppy disks</a></li>
     </ul>
 
-    <p class="wondering">Wondering how SvarDOS is built? You might want to take a look at the <a href="https://osdn.net/projects/svardos/scm/svn/tree/head/">project's SVN</a>, where all the build-related files are stored.</p>
+    <p class="wondering">Wondering how SvarDOS is built? Take a look at the <a href="https://osdn.net/projects/svardos/scm/svn/tree/head/">project's SVN</a>, where all the build-related files are stored.</p>
