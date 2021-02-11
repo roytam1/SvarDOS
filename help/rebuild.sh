90e8b9d7c186179016588b7d1042557a095b7432
mateuszviste <mateuszviste@911cea91-c70f-4353-bd03-772f58fe8c9d>
2021-02-11 12:15:40 +0000

    added articles + versioning is performed by the build script
    git-svn-id: svn://svn.svardos.org/svardos@288 911cea91-c70f-4353-bd03-772f58fe8c9d

--- help/rebuild.sh
+++ help/rebuild.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 set -e
 
+VER=`date +%Y%m%d`
+
+echo "SVARDOS HELP SYSTEM ver $VER" > help-en/title
 ambpack c help-en help-en.amb
+rm help-en/title
+
 mkdir bin
 mkdir help
 mkdir appinfo
 cp help.bat bin
 mv help-*.amb help
-echo "version: `date +%Y%m%d`" >> appinfo/help.lsm
+echo "version: $VER" >> appinfo/help.lsm
 echo "description: SvarDOS help (manual)" >> appinfo/help.lsm
 cp help/help-*.amb ../website/help/
 zip -9rkm help.zip appinfo bin help
