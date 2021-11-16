9b1b6b18a4737630df7cd4fc2aa7f2026d0ae18d
mateuszviste <mateuszviste@911cea91-c70f-4353-bd03-772f58fe8c9d>
2021-11-16 09:30:20 +0000

    fixed "I am process 0" detection under DOS-C
    git-svn-id: svn://svn.svardos.org/svardos@455 911cea91-c70f-4353-bd03-772f58fe8c9d

--- svarcom/trunk/sayonara.c
+++ svarcom/trunk/sayonara.c
@@ -39,7 +39,7 @@ void sayonara(struct rmod_props far *rmod) {
 
   /* detect "I am the origin shell" situations */
   if (rmod->flags & FLAG_PERMANENT) return; /* COMMAND.COM /P */
-  if (rmod->origparent == 0xffff) return; /* original parent set to 0xffff (DOS-C / FreeDOS) */
+  if ((rmod->origparent >> 16) == 0xffff) return; /* original parent seg set to 0xffff (DOS-C / FreeDOS) */
   if (rmod->origenvseg == 0) return; /* no original environment (MSDOS 5/6) */
 
   /* set my parent back to original value */
