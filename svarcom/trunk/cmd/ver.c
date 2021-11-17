edcadb9be9a6a8a344159a2c1305873cacb20b63
mateuszviste <mateuszviste@911cea91-c70f-4353-bd03-772f58fe8c9d>
2021-11-17 20:40:16 +0000

    improved documentation
    git-svn-id: svn://svn.svardos.org/svardos@468 911cea91-c70f-4353-bd03-772f58fe8c9d

--- svarcom/trunk/cmd/ver.c
+++ svarcom/trunk/cmd/ver.c
@@ -76,8 +76,8 @@ static int cmd_ver(struct cmd_funcparam *p) {
     outputnl("This software is distributed under the terms of the MIT license.");
     outputnl("Copyright (C) " COPYRDATE " Mateusz Viste");
     outputnl("");
-    outputnl("Prace te dedykuje Milenie i Mojmirowi. Zycze wam, abyscie w dalszym zyciu");
-    outputnl("potrafili wlasciwie docenic wartosc czasow minionych i czerpali radosc z");
+    outputnl("Prace te dedykuje Milenie i Mojmirowi. Zycze wam, abyscie w swoim zyciu");
+    outputnl("potrafili docenic wartosc czasow minionych, jednoczesnie czerpiac radosc z");
     outputnl("prostych przyjemnosci dnia codziennego.  Lair, jesien 2021.");
     return(-1);
   }
