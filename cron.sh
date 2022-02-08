98abea16c25a4a331392468d29417d82c729eda4
mateusz.viste <mateusz.viste@911cea91-c70f-4353-bd03-772f58fe8c9d>
2022-02-08 17:01:35 +0000

    added a cron script for periodic checkouts and written some stubs of internal documentation related to the build server
    git-svn-id: svn://svn.svardos.org/svardos@636 911cea91-c70f-4353-bd03-772f58fe8c9d

---
+++ cron.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+#
+# this script looks for the presence of /tmp/svardos_repo_changed.flag file
+# and rebuilds the packages index if it exists (then deletes the flag).
+#
+# it is supposed to be called periodically from within a cron job.
+#
+# the /tmp/svardos_repo_changed.flag file is expected to be created by an
+# svn post-commit hook whenever something in the svn repo changes.
+
+
+FLAGFILE="/tmp/svardos_repo_changed.flag"
+SVNREPODIR="/srv/svardos"
+
+# if flag does not exist, exit
+if [ ! -f "$FLAGFILE" ] ; then
+  exit 0
+fi
+
+
+# repo changed! delete the flag file, refresh the local copy of the repo and rebuild packages index
+rm "$FLAGFILE"
+svn up "$SVNREPODIR"
+php "$SVNREPODIR/buildidx/buildidx.php" ""$SVNREPODIR/packages/"
+
+exit 0
