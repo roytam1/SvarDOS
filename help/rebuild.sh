48c5e195835fe9f0d7b1b8a9046b9ae694a36cda
mateusz.viste <mateusz.viste@911cea91-c70f-4353-bd03-772f58fe8c9d>
2022-02-12 20:36:24 +0000

    unicode.map is not stored in svn, and title version autogenerated
    git-svn-id: svn://svn.svardos.org/svardos@662 911cea91-c70f-4353-bd03-772f58fe8c9d

--- help/rebuild.sh
+++ help/rebuild.sh
@@ -3,6 +3,7 @@
 # builds AMB-based help files for each available language
 # this is a Linux shell script that requires following tools to be in path:
 #  - ambpack
+#  - utf8tocp
 #  - zip
 #
 
@@ -10,10 +11,17 @@ set -e
 
 VER=`date +%Y%m%d`
 
-# amb-pack all languages
-for d in ./help-?? ; do
-ambpack cc $d $d.amb
-done
+##### amb-pack all languages #####
+
+# EN
+echo "SVARDOS HELP SYSTEM ver $VER" > help-en/title
+ambpack c help-en help-en.amb
+
+# DE
+echo "SVARDOS-HILFESYSTEM ver $VER" > help-de/title
+utf8tocp -d 858 help-de/unicode.map
+ambpack cc help-de help-de.amb
+rm help-de/unicode.map
 
 mkdir bin
 mkdir help
@@ -22,7 +30,6 @@ cp help.bat bin
 mv help-*.amb help
 echo "version: $VER" >> appinfo/help.lsm
 echo "description: SvarDOS help (manual)" >> appinfo/help.lsm
-cp help/help-*.amb ../website/help/
 zip -9rkDX -m help.zip appinfo bin help
 rmdir appinfo bin help
 mv help.zip ../packages/
