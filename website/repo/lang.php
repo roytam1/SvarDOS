fb482c1b6141fbd050a260445b8b81fff68443c0
bttr <bttr@911cea91-c70f-4353-bd03-772f58fe8c9d>
2022-02-17 21:05:15 +0000

    added German translation to repo interface
    git-svn-id: svn://svn.svardos.org/svardos@815 911cea91-c70f-4353-bd03-772f58fe8c9d

--- website/repo/lang.php
+++ website/repo/lang.php
@@ -15,6 +15,10 @@ $MSG = array();
 $CP_UTF['en'] = array();
 $CP_ENC['en'] = array();
 
+// DE (cp850? -> FIXME)
+$CP_UTF['de'] = array();
+$CP_ENC['de'] = array();
+
 // PL (mazovia)
 $CP_UTF['pl'] = array('Ą',   'Ć',   'Ę',   'Ł',   'Ń',   'Ó',   'Ś',   'Ż',   'Ź',   'ą',   'ć',   'ę',   'ł',   'ń',   'ó',   'ś',   'ż',   'ź');
 $CP_ENC['pl'] = array("\x8f","\x95","\x90","\x9c","\xa5","\xa3","\x98","\xa1","\xa0","\x86","\x8d","\x91","\x92","\xa4","\xa2","\x9e","\xa7","\xa6");
@@ -23,33 +27,43 @@ $CP_ENC['pl'] = array("\x8f","\x95","\x90","\x9c","\xa5","\xa3","\x98","\xa1","\
 // *** MESSAGES ***************************************************************
 
 $MSG['NO_MATCHING_PKG']['en'] = 'No matching package found';
+$MSG['NO_MATCHING_PKG']['de'] = 'Kein passendes Paket gefunden';
 $MSG['NO_MATCHING_PKG']['pl'] = 'Nie znaleziono żadnego pasującego pakietu';
 
 $MSG['PKG_NOT_FOUND']['en'] = 'ERROR: package not found on server';
+$MSG['PKG_NOT_FOUND']['de'] = 'FEHLER: Paket nicht auf dem Server gefunden';
 $MSG['PKG_NOT_FOUND']['pl'] = 'BŁĄD: Nie znaleziono pakietu na serwerze';
 
 $MSG['VER']['en'] = 'ver:';
+$MSG['VER']['de'] = 'Ver:';
 $MSG['VER']['pl'] = 'wer:';
 
 $MSG['SIZE']['en'] = 'size:';
+$MSG['SIZE']['de'] = 'Größe:';
 $MSG['SIZE']['pl'] = 'rozmiar:';
 
 $MSG['ALT_VERS']['en'] = 'alt versions:';
+$MSG['ALT_VERS']['de'] = 'alt. Versionen:';
 $MSG['ALT_VERS']['pl'] = 'alt. wersje:';
 
 $MSG['PACKAGE']['en'] = 'PACKAGE';
+$MSG['PACKAGE']['de'] = 'PAKET';
 $MSG['PACKAGE']['pl'] = 'PAKIET';
 
 $MSG['INSTALLED']['en'] = 'INSTALLED (LOCAL)';
+$MSG['INSTALLED']['de'] = 'INSTALLIERT (LOKAL)';
 $MSG['INSTALLED']['pl'] = 'ZAINSTALOWANY (LOKALNY)';
 
 $MSG['AVAILABLE']['en'] = 'AVAILABLE (REMOTE)';
+$MSG['AVAILABLE']['de'] = 'VERFÜGBAR (ONLINE)';
 $MSG['AVAILABLE']['pl'] = 'DOSTĘPNY (ZDALNY)';
 
 $MSG['NO_UPDATES']['en'] = 'no available updates';
+$MSG['NO_UPDATES']['de'] = 'keine Aktualisierungen verfügbar';
 $MSG['NO_UPDATES']['pl'] = 'brak dostępnych aktualizacji';
 
 $MSG['FOUND_DIFFER']['en'] = 'found differing packages:';
+$MSG['FOUND_DIFFER']['de'] = 'abweichende Pakete gefunden:';
 $MSG['FOUND_DIFFER']['pl'] = 'znalezionych różnic w pakietach:';
 
 ?>
