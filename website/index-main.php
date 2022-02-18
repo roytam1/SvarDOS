dff557e873dd9253a41fc081f025961cf9715b19
mateusz.viste <mateusz.viste@911cea91-c70f-4353-bd03-772f58fe8c9d>
2022-02-18 15:15:32 +0000

    removed link to the TODO, eventually this todo will be gone, only ticketing should be used for tracking issues
    git-svn-id: svn://svn.svardos.org/svardos@858 911cea91-c70f-4353-bd03-772f58fe8c9d

--- website/index-main.php
+++ website/index-main.php
@@ -21,7 +21,7 @@
 
     <h2>Community and help</h2>
 
-    <p>Need to get in touch? Wish to submit some packages, translate SvarDOS to your language, or otherwise contribute? Or maybe you'd like some information about SvarDOS? The project has a <a href="https://lists.osdn.me/mailman/listinfo/svardos-users">mailing list</a> just for that. You may also wish to take a look at the <a href="phpamb.php?fname=help/help-en&amp;f=todo.ama">project's "todo list"</a> and <a href="https://osdn.net/projects/svardos/ticket/">ticket list</a>.</p>
+    <p>Need to get in touch? Wish to submit some packages, translate SvarDOS to your language, or otherwise contribute? Or maybe you'd like some information about SvarDOS? The project has a <a href="https://lists.osdn.me/mailman/listinfo/svardos-users">mailing list</a> just for that. You may also wish to take a look at the project's <a href="https://osdn.net/projects/svardos/ticket/">ticket list</a>.</p>
 
     <?php // the "default" build proposed on the main page is read from default_build.txt
     $lastver = trim(file_get_contents('default_build.txt'));
