38b0bcd5797029dd9e39643beb0de0c4d0e336bc
mateusz.viste <mateusz.viste@911cea91-c70f-4353-bd03-772f58fe8c9d>
2023-02-08 20:44:17 +0000

    frozen ver 20230208 to tags
    git-svn-id: svn://svn.svardos.org/svardos@1189 911cea91-c70f-4353-bd03-772f58fe8c9d

---
+++ pkg/tags/20230208/kprintf.h
@@ -0,0 +1,11 @@
+/*
+ * The following is a macro-like function that uses kitten instead
+ * of directly doing printf.
+ */
+
+#ifndef kprintf_sentinel
+#define kprintf_sentinel
+
+void kitten_printf(short x, short y, ...);
+
+#endif
---
+++ pkg/tags/20230208/pkgrem.h
@@ -0,0 +1,9 @@
+/*
+ * This file is part of the SvarDOS project.
+ * Copyright (C) Mateusz Viste 2012-2021
+ */
+
+#ifndef pkgrem_sentinel
+#define pkgrem_sentinel
+int pkgrem(const char *pkgname, const char *dosdir);
+#endif
