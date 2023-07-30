ec62b85a89d19169b5c53b7c5d26de1e3f4e73d8
mateusz.viste <mateusz.viste@911cea91-c70f-4353-bd03-772f58fe8c9d>
2023-07-30 10:01:17 +0000

    improved doc
    git-svn-id: svn://svn.svardos.org/svardos@1371 911cea91-c70f-4353-bd03-772f58fe8c9d

--- svarlang.lib/trunk/svarlang.h
+++ svarlang.lib/trunk/svarlang.h
@@ -47,13 +47,17 @@ int svarlang_load(const char *fname, const char *lang);
 
 /* tries loading lang strings from a file located in the executable's
  * directory that is named like the executable but with an *.LNG extension.
+ * this is certainly the most practical way of loading svarlang.
  * selfexe should point to the executable's full filename path (either relative
- * or absolute). You may want to pass argv[0] or __argv[0] there. */
+ * or absolute). You may want to pass argv[0] or __argv[0] there. example:
+ *
+ * svarlang_autoload_exepath(argv[0], getenv("LANG"));
+ */
 int svarlang_autoload_exepath(const char *selfexe, const char *lang);
 
 /* this looks in a list of paths separated by ';' to locate a translation file
- * for progname. this is usually called only by "CORE" SvarDOS / FreeDOS
- * programs. typical example:
+ * for progname. this might be called by some FreeDOS programs that rely on the
+ * NLSPATH environment variable for locating strings. example:
  *
  * svarlang_autoload_pathlist("myprog", getenv("NLSPATH"), getenv("LANG"));
  */
