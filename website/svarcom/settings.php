69ce614b67ed09b79a9a083dac7941630d2ead4b
mateusz.viste <mateusz.viste@911cea91-c70f-4353-bd03-772f58fe8c9d>
2023-08-01 18:29:58 +0000

    fixed quotes
    git-svn-id: svn://svn.svardos.org/svardos@1405 911cea91-c70f-4353-bd03-772f58fe8c9d

--- website/svarcom/settings.php
+++ website/svarcom/settings.php
@@ -5,7 +5,7 @@ $PROJNAME = 'SvarCOM';
 $SHORT = 'the SvarDOS command interpreter';
 
 $LONGHTML = "
-<p>SvarCOM is the SvarDOS command line interpreter, known usually under the name "COMMAND.COM". It is designed and maintained by Mateusz Viste, and distributed under the terms of the MIT license.</p>
+<p>SvarCOM is the SvarDOS command line interpreter, known usually under the name \"COMMAND.COM\". It is designed and maintained by Mateusz Viste, and distributed under the terms of the MIT license.</p>
 
 <p>For the time being, it is a work-in-progress project that - although functional - is not entirely polished yet and might miss a few bits and pieces. SvarCOM version 2022.x must be considered a \"preview\" version. See the documentation included in the zip file for details.</p>
 
