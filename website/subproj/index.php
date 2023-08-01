6d308d5bd9f933e458110e71cf01a3d27f9df833
mateusz.viste <mateusz.viste@911cea91-c70f-4353-bd03-772f58fe8c9d>
2023-08-01 14:43:46 +0000

    preparing a sved page
    git-svn-id: svn://svn.svardos.org/svardos@1399 911cea91-c70f-4353-bd03-772f58fe8c9d

---
+++ website/subproj/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html><?php include 'settings.php' ?>
+<html lang="en">
+  <head>
+    <title><?php echo $PROJNAME; ?></title>
+    <meta name="keywords" content="svardos,command.com,shell">
+    <meta name="author" content="Mateusz Viste">
+    <meta name="robots" content="index, follow">
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+
+  <h1><?php echo $PROJNAME; ?></h1>
+  <p class="copyr"><?php echo $SHORT ?></p>
+
+  <p><?php echo $LONG; ?></p>
+
+  <h2>DOWNLOAD</h2>
+
+  <table>
+  <?php
+    $flist = scandir("./", SCANDIR_SORT_DESCENDING);
+    foreach ($flist as $f) {
+      if (strpos($f, '.zip') == false) continue;
+      if (!stristr($f, 'svarcom')) continue;
+      $fsz = intval(filesize($f) / 1024);
+      echo "<tr><td><a href=\"{$f}\">{$f}</a></td><td class=\"siz\"> {$fsz} KiB</td></tr>\n";
+    }
+  ?>
+  </table>
+
+  <h2>LICENSE (MIT)</h2>
+
+  <p>Copyright &copy; <?php echo $COPYRDATE; ?> Mateusz Viste</p>
+
+  <p>Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:</p>
+
+  <p>- The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.</p>
+
+  <p>- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.</p>
+
+  </body>
+</html>
---
+++ website/sved/settings.php
@@ -0,0 +1,15 @@
+<?php
+
+$PROJNAME = 'Sved';
+$SHORT = 'text editor';
+
+$LONG = '
+The SvarDOS editor (SVED) is designed for basic editing of configuration files
+and such. It is NOT meant to be a full-featured text editor. On the pro side,
+it has a low memory footprint and is only a couple kilobytes big, which makes
+it a good fit for bootdisks or systems with extremely limited ressources.
+';
+
+$COPYRDATE = '2023';
+
+?>
