dd0e98dbd7060d02cab39d626e96f5bb3d00b8aa
mateusz.viste <mateusz.viste@911cea91-c70f-4353-bd03-772f58fe8c9d>
2023-08-01 20:04:10 +0000

    migrated svarlang web page to the common subproj template
    git-svn-id: svn://svn.svardos.org/svardos@1406 911cea91-c70f-4353-bd03-772f58fe8c9d

---
+++ website/svarlang/settings.php
@@ -0,0 +1,14 @@
+<?php
+
+$PROJNAME = 'SvarLANG';
+$SHORT = 'the SvarDOS l18n library';
+
+$LONGHTML = '
+<p>SvarLANG is a C library and toolset for enabling DOS applications to easily support multiple languages. It is part of the SvarDOS project and is designed with lightness in mind. Requires the OpenWatcom suite.</p>
+
+<p>All documentation is available in the distributed archive.</p>
+';
+
+$COPYRDATE = '2021-2023';
+
+?>
