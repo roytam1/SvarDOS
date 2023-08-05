2e320daecdb3f656968f64353c32983a8ad93510
mateusz.viste <mateusz.viste@911cea91-c70f-4353-bd03-772f58fe8c9d>
2023-08-05 20:49:13 +0000

    optimized PL translation for smaller size + added russian translations
    git-svn-id: svn://svn.svardos.org/svardos@1432 911cea91-c70f-4353-bd03-772f58fe8c9d

--- sved/trunk/nls/regen.bat
+++ sved/trunk/nls/regen.bat
@@ -2,9 +2,11 @@
 
 utf8tocp 437 en_utf8.txt > en.txt
 utf8tocp maz pl_utf8.txt > pl.txt
+utf8tocp 866 ru_utf8.txt > ru.txt
 
-..\svarlang\tlumacz en pl
+..\svarlang\tlumacz en pl ru
 move /y out.lng ..\sved.lng
 move /y deflang.c ..
 
 del ??.txt
+:: 406
