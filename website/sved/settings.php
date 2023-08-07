5ac1043c37b136bd4497a7981ea52c647079f9a2
mateusz.viste <mateusz.viste@911cea91-c70f-4353-bd03-772f58fe8c9d>
2023-08-07 21:43:18 +0000

    updated the features list
    git-svn-id: svn://svn.svardos.org/svardos@1460 911cea91-c70f-4353-bd03-772f58fe8c9d

--- website/sved/settings.php
+++ website/sved/settings.php
@@ -12,13 +12,14 @@ it a good fit for bootdisks or systems with extremely limited resources.</p>
 <ul>
 <li>runs comfortably on a 8086-class PC with 256 KiB of RAM</li>
 <li>auto-detects color and monochrome video modes</li>
-<li>supports unusual text modes like 80x43, 80x50, 40x25...</li>
+<li>supports unusual text modes like 80x43, 80x50, 132x60, 40x25...</li>
 <li>multilingual UI</li>
 <li>only 7 KiB of disk footprint</li>
 <li>screen estate dedicated to text (no stupid frames, menus and such)</li>
 <li>loads files larger than 64 KiB</li>
-<li>no limit on line lengths</li>
+<li>no line length limit</li>
 <li>can load up to 10 files simultaneously</li>
+<li>COPY/CUT/PASTE between files</li>
 <li>handles CR/LF and LF line endings and can convert between them</li>
 </ul>
 ';
