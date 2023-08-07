7aa3bce4c01a3df50921665b315edf5d5485f6fe
mateusz.viste <mateusz.viste@911cea91-c70f-4353-bd03-772f58fe8c9d>
2023-08-07 20:56:24 +0000

    added french translations
    git-svn-id: svn://svn.svardos.org/svardos@1456 911cea91-c70f-4353-bd03-772f58fe8c9d

--- sved/trunk/nls/regen.bat
+++ sved/trunk/nls/regen.bat
@@ -1,12 +1,12 @@
 @ECHO OFF
 
 utf8tocp 437 en_utf8.txt > en.txt
+utf8tocp 850 fr_utf8.txt > fr.txt
 utf8tocp maz pl_utf8.txt > pl.txt
 utf8tocp 866 ru_utf8.txt > ru.txt
 
-..\svarlang\tlumacz en pl ru
+..\svarlang\tlumacz en fr pl ru
 move /y out.lng ..\sved.lng
 move /y deflang.c ..
 
 del ??.txt
-:: 406
