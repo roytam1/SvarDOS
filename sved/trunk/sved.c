f110fac23c974e4e91f72e196a1745ae51487fe6
mateusz.viste <mateusz.viste@911cea91-c70f-4353-bd03-772f58fe8c9d>
2023-08-13 19:49:18 +0000

    bumped version
    git-svn-id: svn://svn.svardos.org/svardos@1473 911cea91-c70f-4353-bd03-772f58fe8c9d

--- sved/trunk/sved.c
+++ sved/trunk/sved.c
@@ -34,7 +34,7 @@
 #include "svarlang\svarlang.h"
 
 
-#define PVER "2023.2"
+#define PVER "2023.3"
 #define PDATE "2023"
 
 /*****************************************************************************
