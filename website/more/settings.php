c4340bf94ee12ce9de6e900e47f6a3f9ca11b9e7
mateusz.viste <mateusz.viste@911cea91-c70f-4353-bd03-772f58fe8c9d>
2023-08-16 21:26:38 +0000

    fixed link
    git-svn-id: svn://svn.svardos.org/svardos@1495 911cea91-c70f-4353-bd03-772f58fe8c9d

--- website/more/settings.php
+++ website/more/settings.php
@@ -4,9 +4,7 @@ $PROJNAME = 'SvarDOS MORE';
 $SHORT = 'displays output one screen at a time';
 
 $LONGHTML = '
-<p>SvarDOS MORE is a filter that displays output one screen at a time. It is part
-of the SvarDOS project <http://svardos.org>, and as such it shares SvarDOS\'
-principles of minimalism, compactness and bare-bones functionality. It is:</p>
+<p>SvarDOS MORE is a filter that displays output one screen at a time. It is part of the SvarDOS project, and as such it shares SvarDOS\' principles of minimalism, compactness and bare-bones functionality. It is:</p>
 
 <ul>
 <li>multilingual (use the LANG variable, eg. "SET LANG=PL" switches to Polish)</li>
@@ -21,7 +19,7 @@ principles of minimalism, compactness and bare-bones functionality. It is:</p>
 </pre>
 
 <p>SvarDOS MORE is hand crafted in x86 assembly and built using the excellent
-A72 assembler by Mr R. Swan <https://github.com/swanlizard/a72>.</p>
+<a href="https://github.com/swanlizard/a72">A72 assembler</a> by Mr R. Swan.</p>
 ';
 
 $COPYRDATE = '2023';
