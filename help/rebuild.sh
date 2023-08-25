a7da388a371616323b1e17d3f5e6719c5de738f4
mateusz.viste <mateusz.viste@911cea91-c70f-4353-bd03-772f58fe8c9d>
2023-08-25 20:45:43 +0000

    optimize SVP package with advzip
    git-svn-id: svn://svn.svardos.org/svardos@1514 911cea91-c70f-4353-bd03-772f58fe8c9d

--- help/rebuild.sh
+++ help/rebuild.sh
@@ -2,9 +2,10 @@
 #
 # builds AMB-based help files for each available language
 # this is a Linux shell script that requires following tools to be in path:
-#  - ambpack - http://amb.osdn.io/
+#  - ambpack  - http://amb.osdn.io/
 #  - utf8tocp - https://utf8tocp.sourceforge.io/
-#  - zip
+#  - zip      - https://infozip.sourceforge.net/
+#  - advzip   - http://www.advancemame.it/
 #
 
 set -e
@@ -14,18 +15,18 @@ VER=`date +%Y%m%d`
 ##### amb-pack all languages #####
 
 # EN
-echo "SVARDOS HELP SYSTEM ver $VER" > help-en/title
+echo "SVARDOS HELP SYSTEM [$VER]" > help-en/title
 ambpack c help-en help-en.amb
 
 # DE
-echo "SVARDOS-HILFESYSTEM ver $VER" > help-de/title
+echo "SVARDOS-HILFESYSTEM [$VER]" > help-de/title
 cp -n help-en/* help-de/
 utf8tocp -d 858 help-de/unicode.map
 ambpack cc help-de help-de.amb
 rm help-de/unicode.map
 
 # BR
-echo "SISTEMA DE AJUDA DO SVARDOS ver $VER" > help-br/title
+echo "SISTEMA DE AJUDA DO SVARDOS [$VER]" > help-br/title
 cp -n help-en/* help-br/
 utf8tocp -d 858 help-br/unicode.map
 ambpack cc help-br help-br.amb
@@ -40,3 +41,6 @@ echo "version: $VER" >> appinfo/help.lsm
 echo "description: SvarDOS help (manual)" >> appinfo/help.lsm
 zip -9rkDX -m help-$VER.svp appinfo bin help
 rmdir appinfo bin help
+
+# repack the ZIP file with advzip for extra space saving
+advzip -zp4k -i128 help-$VER.svp
