f954e8357a5294e38bb9f4e9e7626fb9aac4ff7b
bernd.boeckmann <bernd.boeckmann@911cea91-c70f-4353-bd03-772f58fe8c9d>
2023-08-27 18:11:55 +0000

    Add german translation for sved.
    git-svn-id: svn://svn.svardos.org/svardos@1529 911cea91-c70f-4353-bd03-772f58fe8c9d

--- sved/trunk/nls/regen.bat
+++ sved/trunk/nls/regen.bat
@@ -1,12 +1,13 @@
 @ECHO OFF
 
 utf8tocp 437 en_utf8.txt > en.txt
+utf8tocp 850 de_utf8.txt > de.txt
 utf8tocp 850 fr_utf8.txt > fr.txt
 utf8tocp maz pl_utf8.txt > pl.txt
 utf8tocp 866 ru_utf8.txt > ru.txt
 utf8tocp 857 tr_utf8.txt > tr.txt
 
-..\svarlang\tlumacz en fr pl ru tr
+..\svarlang\tlumacz en de fr pl ru tr
 move /y out.lng ..\sved.lng
 move /y deflang.c ..
 
