408378c93c39d8e136f34e818bc6da155633109f
bernd.boeckmann <bernd.boeckmann@911cea91-c70f-4353-bd03-772f58fe8c9d>
2023-11-14 19:17:50 +0000

    fix wmincrt settings.php
    git-svn-id: svn://svn.svardos.org/svardos@1570 911cea91-c70f-4353-bd03-772f58fe8c9d

--- website/wmincrt/settings.php
+++ website/wmincrt/settings.php
@@ -4,8 +4,8 @@ $PROJNAME = 'WMINCRT';
 $SHORT = 'a minimalist startup code for Open Watcom';
 
 $LONGHTML = '
-<p>until a proper description is placed here, please have a look at the 
-<a href=http://svn.svardos.org/listing.php?repname=SvarDOS&path=%2Fwmincrt>SVN repository</a>
+<p>Until a proper description is placed here, please have a look at the 
+<a href="http://svn.svardos.org/listing.php?repname=SvarDOS&path=%2Fwmincrt">SVN repository</a>.
 </p>
 ';
 
