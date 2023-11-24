764c315d8476a3c51c892616fb255db3263c63ad
mateusz.viste <mateusz.viste@911cea91-c70f-4353-bd03-772f58fe8c9d>
2023-11-24 17:46:47 +0000

    frozen ver 20231124 to tags
    git-svn-id: svn://svn.svardos.org/svardos@1611 911cea91-c70f-4353-bd03-772f58fe8c9d

---
+++ pkg/tags/20231124/inf.c
@@ -0,0 +1,81 @@
+/* This file is part of the FDNPKG project. It is an adaptation of the
+ * "zpipe.c" example of zlib's inflate() and deflate() usage.
+ *
+ * Not copyrighted -- provided to the public domain
+ * original version 1.4  11 December 2005  Mark Adler
+ * adaptations for FDNPKG integration by Mateusz Viste, 2015
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+#include "crc32.h"
+#include "zlib\zlib.h"
+
+#include "inf.h"
+
+/* Decompress from file source to file dest until stream ends or EOF.
+ * inf() returns Z_OK on success, Z_MEM_ERROR if memory could not be allocated
+ * for processing, Z_DATA_ERROR if the deflate data is invalid or incomplete,
+ * Z_VERSION_ERROR if the version of zlib.h and the version of the library
+ * linked do not match, or Z_ERRNO if there is an error reading or writing the
+ * files. */
+int inf(FILE *source, FILE *dest, unsigned char *buffin, unsigned short buffinsz, unsigned char *buffout, unsigned short buffoutsz, unsigned long *cksum, long streamlen) {
+  int ret;
+  unsigned int have;
+  z_stream strm;
+
+  /* allocate inflate state */
+  strm.zalloc = Z_NULL;
+  strm.zfree = Z_NULL;
+  strm.opaque = Z_NULL;
+  strm.avail_in = 0;
+  strm.next_in = Z_NULL;
+  ret = inflateInit2(&strm, -15); /* according to the zlib doc, passing -15 to inflateInit2() means "this is a raw deflate stream" (as opposed to a zlib- or gz- wrapped stream) */
+  if (ret != Z_OK) return(ret);
+
+  /* decompress until deflate stream ends or end of file */
+  do {
+    strm.avail_in = fread(buffin, 1, (streamlen > buffinsz ? buffinsz : streamlen), source);
+    if (ferror(source)) {
+      (void)inflateEnd(&strm);
+      return(Z_ERRNO);
+    }
+    streamlen -= strm.avail_in;
+    if (strm.avail_in == 0) break;
+    strm.next_in = buffin;
+
+    /* run inflate() on input until output buffer not full */
+    do {
+      strm.avail_out = buffoutsz;
+      strm.next_out = buffout;
+      ret = inflate(&strm, Z_NO_FLUSH);
+      switch (ret) {
+        case Z_NEED_DICT:
+          ret = Z_DATA_ERROR;     /* and fall through */
+        case Z_DATA_ERROR:
+        case Z_MEM_ERROR:
+          (void)inflateEnd(&strm);
+          return(ret);
+      }
+      have = buffoutsz - strm.avail_out;
+      if ((fwrite(buffout, 1, have, dest) != have) || (ferror(dest))) {
+        (void)inflateEnd(&strm);
+        return(Z_ERRNO);
+      }
+      /* feed the CRC32 */
+      crc32_feed(cksum, buffout, have);
+    } while (strm.avail_out == 0);
+
+    /* done when inflate() says it's done */
+  } while (ret != Z_STREAM_END);
+
+  /* clean up and return */
+  (void)inflateEnd(&strm);
+
+  if (Z_STREAM_END) {
+    return(Z_OK);
+  } else {
+    return(Z_DATA_ERROR);
+  }
+}
---
+++ pkg/tags/20231124/loadconf.c
@@ -0,0 +1,158 @@
+/*
+ * This file is part of pkg (SvarDOS).
+ * Copyright (C) 2012-2021 Mateusz Viste
+ */
+
+#include <stdio.h>  /* printf(), fclose(), fopen()... */
+#include <string.h> /* strcasecmp() */
+#include <stdlib.h> /* malloc(), free() */
+
+#include "helpers.h" /* slash2backslash(), removeDoubleBackslashes()... */
+#include "kprintf.h"
+
+#include "loadconf.h"
+
+
+void freeconf(struct customdirs **dirlist) {
+  struct customdirs *curpos;
+  /* free the linked list of custom dirs */
+  while (*dirlist != NULL) {
+    curpos = *dirlist;
+    *dirlist = (*dirlist)->next;
+    free(curpos);
+  }
+}
+
+
+static int checkfordoubledirlist(const struct customdirs *dirlist) {
+  struct customdirs *curpos;
+  for (; dirlist != NULL; dirlist = dirlist->next) {
+    for (curpos = dirlist->next; curpos != NULL; curpos = curpos->next) {
+      if (strcasecmp(curpos->name, dirlist->name) == 0) {
+        kitten_printf(7, 0, curpos->name); /* "ERROR: custom dir '%s' is listed twice!" */
+        puts("");
+        return(-1);
+      }
+    }
+  }
+  return(0);
+}
+
+
+/* validates dirlist entries: check that they are absolute paths and are not using restricted names */
+static int validatedirlist(const struct customdirs *dirlist) {
+  for (; dirlist != NULL; dirlist = dirlist->next) {
+    /* the location must be at least 3 characters long to be a valid absolute path (like 'c:\')*/
+    if (strlen(dirlist->location) < 3) {
+      kitten_printf(7, 15, dirlist->name); /* "ERROR: custom dir '%s' is not a valid absolute path!" */
+      puts("");
+      return(-1);
+    }
+    /* is it a valid absolute path? should start with [a..Z]:\ */
+    if ((dirlist->location[1] != ':') ||
+       ((dirlist->location[2] != '/') && (dirlist->location[2] != '\\')) ||
+       (((dirlist->location[0] < 'a') || (dirlist->location[0] > 'z')) && ((dirlist->location[0] < 'A') || (dirlist->location[0] > 'Z')))) {
+      kitten_printf(7, 15, dirlist->name); /* "ERROR: custom dir '%s' is not a valid absolute path!" */
+      puts("");
+      return(-1);
+    }
+    /* check for forbidden names */
+    if ((strcasecmp(dirlist->name, "appinfo") == 0) ||
+        (strcasecmp(dirlist->name, "bin") == 0) ||
+        (strcasecmp(dirlist->name, "doc") == 0) ||
+        (strcasecmp(dirlist->name, "help") == 0) ||
+        (strcasecmp(dirlist->name, "nls") == 0) ||
+        (strcasecmp(dirlist->name, "packages") == 0)) {
+      kitten_printf(7, 16, dirlist->name); /* "ERROR: custom dir '%s' is a reserved name!" */
+      puts("");
+      return(-1);
+    }
+  }
+  return(0);
+}
+
+
+/* add (and allocates) a new custom dir entry to dirlist. Returns 0 on success,
+   or non-zero on failure (failures happen on out of memory events). */
+static int addnewdir(struct customdirs **dirlist, const char *name, const char *location) {
+  struct customdirs *newentry;
+  if (strlen(name) >= sizeof(newentry->name)) return(-2);
+  newentry = malloc(sizeof(struct customdirs) + strlen(location) + 1);
+  if (newentry == NULL) return(-1);
+  strcpy(newentry->name, name);
+  strcpy(newentry->location, location);
+  newentry->next = *dirlist;
+  *dirlist = newentry;
+  return(0);
+}
+
+
+int loadconf(const char *dosdir, struct customdirs **dirlist) {
+  FILE *fd;
+  char *value = NULL;
+  char token[512];
+  int nline = 0;
+
+  snprintf(token, sizeof(token), "%s\\cfg\\pkg.cfg", dosdir);
+  fd = fopen(token, "r");
+  if (fd == NULL) {
+    kitten_printf(7, 1, token); /* "ERROR: Could not open config file (%s)!" */
+    puts("");
+    return(-1);
+  }
+
+  *dirlist = NULL;
+
+  /* read the config file line by line */
+  while (freadtokval(fd, token, sizeof(token), &value, ' ') == 0) {
+    nline++;
+
+    /* skip comments and empty lines */
+    if ((token[0] == '#') || (token[0] == 0)) continue;
+
+    if ((value == NULL) || (value[0] == 0)) {
+      kitten_printf(7, 4, nline); /* "Warning: token with empty value on line #%d" */
+      puts("");
+      continue;
+    }
+
+    /* printf("token='%s' ; value = '%s'\n", token, value); */
+    if (strcasecmp(token, "DIR") == 0) { /* custom directory entry */
+      char *location = NULL;
+      int i;
+      /* find nearer space */
+      for (i = 0; (value[i] != ' ') && (value[i] != 0); i++);
+      if (value[i] == 0) {
+        kitten_printf(7, 11, nline); /* "Warning: Invalid 'DIR' directive found at line #%d" */
+        puts("");
+        continue;
+      }
+      value[i] = 0;
+      location = value + i + 1;
+
+      /* add the entry to the list */
+      slash2backslash(location);
+      removeDoubleBackslashes(location);
+      if (location[strlen(location) - 1] != '\\') strcat(location, "\\"); /* make sure to end dirs with a backslash */
+      if (addnewdir(dirlist, value, location) != 0) {
+        kitten_printf(2, 14, "addnewdir"); /* "Out of memory! (%s)" */
+        puts("");
+        freeconf(dirlist);
+        fclose(fd);
+        return(-1);
+      }
+    } else { /* unknown token */
+      kitten_printf(7, 8, token, nline); /* "Warning: Unknown token '%s' at line #%d" */
+      puts("");
+    }
+  }
+  fclose(fd);
+
+  /* perform some validations */
+  if ((checkfordoubledirlist(*dirlist) != 0) || (validatedirlist(*dirlist) != 0)) {
+    freeconf(dirlist);
+    return(-1);
+  }
+
+  return(0);
+}
