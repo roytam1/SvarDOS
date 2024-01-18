6d0e1b8188fcb9686dd88b00c4b3b0e8708d532a
mateusz.viste <mateusz.viste@911cea91-c70f-4353-bd03-772f58fe8c9d>
2024-01-18 14:21:02 +0000

    frozen ver 20240118 to tags
    git-svn-id: svn://svn.svardos.org/svardos@1635 911cea91-c70f-4353-bd03-772f58fe8c9d

---
+++ pkg/tags/20240118/inf.h
@@ -0,0 +1,13 @@
+
+#ifndef INF_H
+#define INF_H
+
+/* Decompress from file source to file dest until stream ends or EOF.
+ * inf() returns Z_OK on success, Z_MEM_ERROR if memory could not be allocated
+ * for processing, Z_DATA_ERROR if the deflate data is invalid or incomplete,
+ * Z_VERSION_ERROR if the version of zlib.h and the version of the library
+ * linked do not match, or Z_ERRNO if there is an error reading or writing the
+ * files. */
+int inf(FILE *source, FILE *dest, unsigned char *buffin, unsigned short buffinsz, unsigned char *buffout, unsigned short buffoutsz, unsigned long *cksum, long streamlen);
+
+#endif
---
+++ pkg/tags/20240118/loadconf.h
@@ -0,0 +1,23 @@
+/*
+ * This file is part of pkg.
+ *
+ * Copyright (C) 2012-2021 Mateusz Viste
+ */
+
+#ifndef loadrepolist_sentinel
+#define loadrepolist_sentinel
+
+struct customdirs {
+  struct customdirs *next;
+  char name[9];
+  char location[1]; /* extended at alloc time */
+};
+
+/* Loads the list of custom directories from the config file
+ * Returns 0 on success, or -1 on failure. */
+int loadconf(const char *dosdir, struct customdirs **dirlist);
+
+/* Free the memory allocated at configuration load. */
+void freeconf(struct customdirs **dirlist);
+
+#endif
