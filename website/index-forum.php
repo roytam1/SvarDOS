684fc8aa4abf491abdfca446dd95979bafb53cf4
mateusz.viste <mateusz.viste@911cea91-c70f-4353-bd03-772f58fe8c9d>
2024-01-19 21:56:48 +0000

    forum title is clickable
    git-svn-id: svn://svn.svardos.org/svardos@1646 911cea91-c70f-4353-bd03-772f58fe8c9d

--- website/index-forum.php
+++ website/index-forum.php
@@ -1,5 +1,7 @@
+<a href="?p=forum" style="color: inherit; font-decoration: none;">
 <h1>SvarDOS community forum</h1>
 <p class="copyr">a place to talk about SvarDOS and other DOS-related things</p>
+</a>
 
 <?php
 include 'mateuszbb.php';
