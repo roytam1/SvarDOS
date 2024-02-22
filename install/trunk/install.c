fdaf7c05340d7c002de952fd5579581f24579b26
mateusz.viste <mateusz.viste@911cea91-c70f-4353-bd03-772f58fe8c9d>
2024-02-22 00:52:55 +0000

    replaced UDVD2 with VIDE-CDD + added some human descriptions to generated CONFIG.SYS
    git-svn-id: svn://svn.svardos.org/svardos@1751 911cea91-c70f-4353-bd03-772f58fe8c9d

--- install/trunk/install.c
+++ install/trunk/install.c
@@ -727,13 +727,28 @@ static void bootfilesgen(char targetdrv, const struct slocales *locales) {
   snprintf(buff, sizeof(buff), "%c:\\TEMP\\CONFIG.SYS", targetdrv);
   fd = fopen(buff, "wb");
   if (fd == NULL) return;
-  fprintf(fd, "LASTDRIVE=Z\r\n"
-              "FILES=40\r\n");
-  fprintf(fd, "DEVICE=C:\\SVARDOS\\HIMEMX.EXE\r\n");
-  fprintf(fd, "DOS=UMB,HIGH\r\n");
-  fprintf(fd, "SHELL=C:\\COMMAND.COM /E:512 /P\r\n");
-  fprintf(fd, "REM COUNTRY=001,%u,C:\\SVARDOS\\CFG\\COUNTRY.SYS\r\n", locales->codepage);
-  fprintf(fd, "REM DEVICE=C:\\DRIVERS\\UDVD2\\UDVD2.SYS /D:SVCD0001 /H\r\n");
+  fprintf(fd, "; SvarDOS kernel configuration\r\n"
+              "\r\n"
+              "; highest allowed drive letter\r\n"
+              "LASTDRIVE=Z\r\n"
+              "\r\n"
+              "; max. number of files that programs are allowed to open simultaneously\r\n"
+              "FILES=25\r\n");
+  fprintf(fd, "\r\n"
+              "; XMS memory driver\r\n"
+              "DEVICE=C:\\SVARDOS\\HIMEMX.EXE\r\n");
+  fprintf(fd, "\r\n"
+              "; try moving DOS to upper memory, then to high memory\r\n"
+              "DOS=UMB,HIGH\r\n");
+  fprintf(fd, "\r\n"
+              "; command interpreter (shell) location and default environment size\r\n"
+              "SHELL=C:\\COMMAND.COM /E:512 /P\r\n");
+  fprintf(fd, "\r\n"
+              "; NLS configuration\r\n"
+              ";COUNTRY=001,%u,C:\\SVARDOS\\CFG\\COUNTRY.SYS\r\n", locales->codepage);
+  fprintf(fd, "\r\n"
+              "; CD-ROM driver initialization\r\n"
+              ";DEVICE=C:\\DRIVERS\\VIDECDD\\VIDE-CDD.SYS /D:SVCD0001\r\n");
   fclose(fd);
   /*** AUTOEXEC.BAT ***/
   snprintf(buff, sizeof(buff), "%c:\\TEMP\\AUTOEXEC.BAT", targetdrv);
