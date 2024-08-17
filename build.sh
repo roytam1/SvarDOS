087bff7e8a79f391387bab8e9cfec79364cbb811
mateusz.viste <mateusz.viste@911cea91-c70f-4353-bd03-772f58fe8c9d>
2024-08-17 20:19:39 +0000

    disabled the EN-only build
    git-svn-id: svn://svn.svardos.org/svardos@1927 911cea91-c70f-4353-bd03-772f58fe8c9d

--- build.sh
+++ build.sh
@@ -335,26 +335,26 @@ zip -mj9 "$CDZIP" "$CDISO"
 # way to build the 360K variant                                               #
 ###############################################################################
 
-rm "$FLOPROOT"/*.cpx
-rm "$FLOPROOT"/install.lng
-rm "$FLOPROOT"/display.exe
-rm "$FLOPROOT"/mode.com
+#rm "$FLOPROOT"/*.cpx
+#rm "$FLOPROOT"/install.lng
+#rm "$FLOPROOT"/display.exe
+#rm "$FLOPROOT"/mode.com
 
 ###############################################################################
 # remove NLS and LNG files from packages to build EN-ONLY builds              #
 ###############################################################################
 
-echo "### remove NLS and LNG files from packages to build EN-ONLY builds"
-for p in $COREPKGS ; do
-  # the || true bit is because zip returns a non-zero exit code on no file match
-  zip -dq "$CDROOT/$p.svp" 'bin/*.lng' 'BIN/*.LNG' 'nls/*' 'NLS/*' || true
-done
+#echo "### remove NLS and LNG files from packages to build EN-ONLY builds"
+#for p in $COREPKGS ; do
+#  # the || true bit is because zip returns a non-zero exit code on no file match
+#  zip -dq "$CDROOT/$p.svp" 'bin/*.lng' 'BIN/*.LNG' 'nls/*' 'NLS/*' || true
+#done
 
-prep_flop 80 2 18 1440 "$PUBDIR" "1.44M-EN_ONLY" "$COREPKGS"
+#prep_flop 80 2 18 1440 "$PUBDIR" "1.44M-EN_ONLY" "$COREPKGS"
 #prep_flop 80 2 21 1680 "$PUBDIR" "1.44M-DMF-EN_ONLY"
-prep_flop 80 2 15 1200 "$PUBDIR" "1.2M-EN_ONLY" "$COREPKGS"
-prep_flop 80 2  9  720 "$PUBDIR" "720K-EN_ONLY" "$COREPKGS"
-prep_flop 40 2  9  360 "$PUBDIR" "360K-EN_ONLY" "$COREPKGS"
+#prep_flop 80 2 15 1200 "$PUBDIR" "1.2M-EN_ONLY" "$COREPKGS"
+#prep_flop 80 2  9  720 "$PUBDIR" "720K-EN_ONLY" "$COREPKGS"
+#prep_flop 40 2  9  360 "$PUBDIR" "360K-EN_ONLY" "$COREPKGS"
 
 
 ###############################################################################
