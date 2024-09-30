5117a3235402a4d0fa6512e246d33f4a161d25da
mateusz.viste <mateusz.viste@911cea91-c70f-4353-bd03-772f58fe8c9d>
2024-09-30 10:56:55 +0000

    added extra tests
    git-svn-id: svn://svn.svardos.org/svardos@2103 911cea91-c70f-4353-bd03-772f58fe8c9d

--- svarlang.lib/trunk/test/build.bat
+++ svarlang.lib/trunk/test/build.bat
@@ -9,3 +9,15 @@ rename out.lng outc.lng
 ..\tlumacz /nocomp en pl
 
 wcl -0 -wx -ox -we -ms test.c deflang.c ..\svarlngs.lib
+
+cd fdisk
+..\..\tlumacz en de es fr it pl tr
+copy out.lng ..\fdisk.lng
+del out.lng
+cd ..
+
+cd tree
+..\..\tlumacz en de es fi lv pt ru tr
+copy out.lng ..\tree.lng
+del out.lng
+cd ..
