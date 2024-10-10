02e4500c25382661b139647df7f49a6bde14fc0d
mateusz.viste <mateusz.viste@911cea91-c70f-4353-bd03-772f58fe8c9d>
2024-10-10 13:04:42 +0000

    moved tests out of trunk
    git-svn-id: svn://svn.svardos.org/svardos@2144 911cea91-c70f-4353-bd03-772f58fe8c9d

--- svarlang.lib/tests/build.bat
+++ svarlang.lib/tests/build.bat
@@ -3,15 +3,15 @@
 IF EXIST OUT.LNG DEL OUT.LNG
 IF EXIST OUTC.LNG DEL OUTC.LNG
 
-..\tlumacz en pl
+..\trunk\tlumacz en pl
 rename out.lng outc.lng
 
-..\tlumacz /nocomp en pl
+..\trunk\tlumacz /nocomp en pl
 
 wcl -0 -wx -ox -we -ms test.c deflang.c ..\svarlngs.lib
 
 cd fdisk
-..\..\tlumacz en de es fr it /nocomp pl tr
+..\..\trunk\tlumacz en de es fr it /nocomp pl tr
 del ..\fdisk.lng
 copy out.lng ..\fdisk.lng
 del ..\deflfdsk.c
@@ -19,29 +19,29 @@ copy deflang.c ..\deflfdsk.c
 del out.lng
 
 del ..\fdisk_de.lng
-..\..\tlumacz /nodef de
+..\..\trunk\tlumacz /nodef de
 copy out.lng ..\fdisk_de.lng
 del out.lng
 cd ..
 
-wcl -0 -wx -ox -we -ms tim.c deflfdsk.c ..\svarlngs.lib
+wcl -0 -wx -ox -we -ms tim.c deflfdsk.c ..\trunk\svarlngs.lib
 
 cd tree
-..\..\tlumacz /nodef en de es fi lv pt ru tr
+..\..\trunk\tlumacz /nodef en de es fi lv pt ru tr
 del ..\tree.lng
 copy out.lng ..\tree.lng
 del out.lng
 cd ..
 
 cd install
-..\..\tlumacz /nodef en br de fr it pl ru si sv tr
+..\..\trunk\tlumacz /nodef en br de fr it pl ru si sv tr
 del ..\install.lng
 copy out.lng ..\install.lng
 del out.lng
 cd ..
 
 cd svarcom
-..\..\tlumacz /nodef en br de fr pl tr
+..\..\trunk\tlumacz /nodef en br de fr pl tr
 del ..\svarcom.lng
 copy out.lng ..\svarcom.lng
 del out.lng
